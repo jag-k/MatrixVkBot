2b3548fa4b6ee43c3a7c3857de272350445f89c8
progserega <progserega@gmail.com>
2018-12-22 21:03:00 +1000

    конфиг

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+server="https://matrix.org"
+username="vk2matrix"
+password="XXXXXX"
+
+data_file="data.json"
+
+log_path="MatrixVkBot.log"
+
+debug=True
