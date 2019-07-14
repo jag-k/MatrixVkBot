9bff5b6c332b81dba87bdb8ca8527ba380862865
progserega <progserega@gmail.com>
2019-07-14 14:40:29 +1000

    по возможности имя комнаты как имя, а не как id

--- bot.py
+++ bot.py
@@ -2398,7 +2398,10 @@ def proccess_vk_message(bot_control_room,room,user,sender_name,m):
       else:
         send_status=True
     if send_status==False:
-      bot_system_message(user,'Ошибка: не смог отправить сообщение в матрицу из ВК в комнату %s'%room)
+      matrix_room_name=get_name_of_matrix_room(room)
+      if matrix_room_name==None:
+        matrix_room_name=room
+      bot_system_message(user,'Ошибка: не смог отправить сообщение в матрицу из ВК в комнату %s'%matrix_room_name)
       log.warning("сообщение было:")
       log.warning(m)
 
